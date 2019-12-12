0cc506c50ed37be6ba176880a36ad16dbd85d451
Xianshuai Shen <v-xiashe@microsoft.com>
2019-12-12 22:08:08 +0800

    uoload img

--- admin/src/views/ProductEdit.vue
+++ admin/src/views/ProductEdit.vue
@@ -6,7 +6,15 @@
         <el-input v-model="model.name"></el-input>
       </el-form-item>
       <el-form-item label="图片地址">
-        <el-input v-model="model.icon"></el-input>
+        <el-upload
+          class="avatar-uploader"
+          :action="$http.defaults.baseURL + '/upload'"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+        >
+          <img v-if="model.icon" :src="model.icon" class="avatar" />
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
@@ -46,7 +54,37 @@ export default {
         type: "success",
         message: "保存成功"
       });
+    },
+    handleAvatarSuccess(){
+      console.log(1111111)
+      // this.model.icon = URL.createObjectURL(file.raw);
     }
   }
 };
 </script>
+
+<style>
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+</style>
